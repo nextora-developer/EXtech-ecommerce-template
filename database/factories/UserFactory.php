cf16432bd2a115d15f8ef7f6042bc44cd4c2ecbf
mingxuanyap <mingxuanyap622@gmail.com>
2026-01-02 02:35:03 +0800

    update

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class UserFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'), // 预设密码：password
+            'remember_token' => Str::random(10),
+        ];
+    }
+
+    /**
+     * 未验证邮箱状态
+     */
+    public function unverified(): static
+    {
+        return $this->state(fn() => [
+            'email_verified_at' => null,
+        ]);
+    }
+}
